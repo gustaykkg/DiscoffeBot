ffe333363645f78e65b05faab1579ad006a3fe1e
Gustavo Monti Selli <gustavomontiselli@gmail.com>
2021-09-16 21:02:40 -0300

    voicestats update

--- events/voiceStats.js
+++ events/voiceStats.js
@@ -10,9 +10,8 @@ client.on('voiceStateUpdate', async (oldState, newState) => {
     if(oldState.member.roles.cache.has(config.roles.movcall) || newState.member.roles.cache.has(config.roles.movcall)){
     if((oldState.member && oldState.member.user.bot) || (newState.member && newState.member.user.bot)) return;
     if(!oldState.channel && newState.channel) {
-        console.log('a')
         Activites.set(oldState.id, Date.now());
-        channel.send({embeds: [{description: `${newState.member}\nentrou na call <#${newState.channelId}>`, color:newState.member.displayHexColor}]})
+       await channel.send({embeds: [{description: `${newState.member}\nentrou na call <#${newState.channelId}>`, color:newState.member.displayHexColor}]})
     }
     let data;
     if(!Activites.has(oldState.id)){
@@ -25,13 +24,13 @@ client.on('voiceStateUpdate', async (oldState, newState) => {
     let duration = Date.now() - data;
     if(oldState.channel && !newState.channel) { // sai call
         Activites.delete(oldState.id);
-        channel.send({embeds: [{description: `${oldState.member}\nsaiu da call <#${oldState.channelId}>`, color:oldState.member.displayHexColor}]})
+        await channel.send({embeds: [{description: `${oldState.member}\nsaiu da call <#${oldState.channelId}>`, color:oldState.member.displayHexColor}]})
         vt.add(`stats.${oldState.guild.id}.${oldState.id}.channels.${oldState.channelId}`, duration);
         vt.set(`stats.${oldState.guild.id}.${oldState.id}.activity`, Date.now());
     }
     else if(oldState.channel && newState.channel){ // troca call
         Activites.set(oldState.id, Date.now());
-        channel.send({embeds: [{description: `${oldState.member}\nsaiu da call <#${oldState.channelId}>\ne foi para <#${newState.channelId}>`, color:oldState.member.displayHexColor}]})
+        await channel.send({embeds: [{description: `${oldState.member}\nsaiu da call <#${oldState.channelId}>\ne foi para <#${newState.channelId}>`, color:oldState.member.displayHexColor}]})
         vt.add(`stats.${oldState.guild.id}.${oldState.id}.channels.${oldState.channelId}`, duration);
         vt.set(`stats.${oldState.guild.id}.${oldState.id}.activity`, Date.now());
     }
